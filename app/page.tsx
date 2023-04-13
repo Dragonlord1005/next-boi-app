c144bce7823ed4601ef5ccfe12a45492930ed9c2
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2023-04-13 18:49:19 +0000

    ✏️ Fixed className being defined as class

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     return(
         <main>
             <div>
-                <h1 class="text-center font-inter font-bold">Hi</h1>
+                <h1 className="text-center font-inter font-bold">Hi</h1>
             </div>
         </main>
     )
